33b26ebebaaff0eb5dc4e3eb1541d06fa16b1854
liangwh <0027015537@iwhalecloud.com>
2020-08-14 16:51:52 +0800

    添加 eureka  restTemplate @LoadBalanced 开启负载均衡

--- leanspringboot/eurekaservice/src/main/java/xyz/liangwh/eurekaservice/EurekaserviceApplication.java
+++ leanspringboot/eurekaservice/src/main/java/xyz/liangwh/eurekaservice/EurekaserviceApplication.java
@@ -2,14 +2,26 @@ package xyz.liangwh.eurekaservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableEurekaClient
+@Configuration
 public class EurekaserviceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(EurekaserviceApplication.class, args);
     }
 
+
+
+    @Bean
+    @LoadBalanced
+    public RestTemplate restTemplate(){
+        return new RestTemplate();
+    }
 }
--- leanspringboot/eurekaservice/src/main/java/xyz/liangwh/eurekaservice/test/TestController.java
+++ leanspringboot/eurekaservice/src/main/java/xyz/liangwh/eurekaservice/test/TestController.java
@@ -1,10 +1,12 @@
 package xyz.liangwh.eurekaservice.test;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
 
@@ -22,12 +24,31 @@ public class TestController {
         String description = discoveryClient.description();
         List<String> services = discoveryClient.getServices();
         List<ServiceInstance> instances = discoveryClient.getInstances("EUREKA-CLIENT-1");
-        return null;
+        return instances;
     }
     @RequestMapping("updatess")
     public boolean updagteServiceStatus(boolean status){
         indicator.setStatus(status);
         return true;
+    }
+
+    @Value("${lwh.data}")
+    private String data;
+
+
+
+    @RequestMapping("get/data")
+    public String getData(){
+        return data;
+    }
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @RequestMapping("lb/data")
+    public String getDataLb(){
+
+        return restTemplate.getForObject("http://EUREKA-CLIENT-1/get/data",String.class);
     }
 
 }
