8ac7c0c7c6202a15df7035665ae596d01f9ca13c
liangwh <0027015537@iwhalecloud.com>
2020-08-18 10:09:10 +0800

    zuul配置

--- leanspringboot/zuul/src/main/java/xyz/liangwh/zuul/ZuulApplication.java
+++ leanspringboot/zuul/src/main/java/xyz/liangwh/zuul/ZuulApplication.java
@@ -2,8 +2,10 @@ package xyz.liangwh.zuul;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
+@EnableZuulProxy
 public class ZuulApplication {
 
     public static void main(String[] args) {
