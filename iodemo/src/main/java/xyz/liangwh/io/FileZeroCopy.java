028da20e8efc3b4e9e0ebeb50558c6d2b34ab1ce
liangwh <0027015537@iwhalecloud.com>
2020-09-16 17:35:36 +0800

    zero copy

---
+++ iodemo/src/main/java/xyz/liangwh/io/FileZeroCopy.java
@@ -0,0 +1,27 @@
+package xyz.liangwh.io;
+
+import lombok.SneakyThrows;
+
+import java.io.RandomAccessFile;
+import java.net.InetSocketAddress;
+import java.net.SocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.MappedByteBuffer;
+import java.nio.channels.FileChannel;
+import java.nio.channels.SocketChannel;
+
+public class FileZeroCopy {
+
+    @SneakyThrows
+    public static void main(String[] args) {
+        RandomAccessFile raf = new RandomAccessFile("E:\\fileid.txt", "r");
+
+        FileChannel rafchannel = raf.getChannel();
+        SocketAddress address;
+        SocketChannel socketChannel = SocketChannel.open(new InetSocketAddress("134.175.130.172",9090));
+        rafchannel.transferTo(0,rafchannel.size(),socketChannel);
+        socketChannel.close();
+        rafchannel.close();
+
+    }
+}
